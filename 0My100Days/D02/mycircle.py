e6726db8d5798890afd22420b04a483f9d56bc5f
AsgeologeekFan <hhufht@outlook.com>
2020-05-24 18:40:44 +0800

    add mycircle.py

---
+++ 0My100Days/D02/mycircle.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# AUTHOR: Haotian Fan   TIME: 2020/5/24
+import math
+
+radius = float(input('请输入圆的半径：'))
+perimeter = 2 * math.pi * radius
+area = math.pi * radius * radius
+print('L: %.2f' % perimeter)
+print('Area: %.2f' % area)
